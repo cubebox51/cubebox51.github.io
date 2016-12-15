daaaa848fd3d438f28810f9fd4d4adcc41610361
cubebox51 <cubebox51@gmail.com>
2016-12-15 12:25:36 -0800

    Update game.py

--- game.py
+++ game.py
@@ -2,5 +2,12 @@ user_input = input("Whats 9 + 10")
 
 if (user_input) == 21:
     print("You Stupid")
+
+if (user_input) == 69:
+    print ("No I Dont")
+
+elif (user_input) == 19:
+    print("YOU SMART OMG")
+
 else:
     print("You Smart")
