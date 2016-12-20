b3188de52a66031fafce1be5d93c46de9c51b16f
cubebox51 <cubebox51@gmail.com>
2016-12-19 22:36:44 -0800

    Update game.py

--- game.py
+++ game.py
@@ -11,3 +11,17 @@ elif (user_input) == 19:
 
 else:
     print("You Smart")
+
+    user_input = input("Whats 9 + 10")
+
+if (user_input) == 21:
+    print("You Stupid")
+
+if (user_input) == 69:
+    print ("No I Dont")
+
+elif (user_input) == 19:
+    print("YOU SMART OMG")
+
+else:
+    print("You Smart")
