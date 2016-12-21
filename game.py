6ad5229321c0e4df3d55008bae2437ac7600d716
cubebox51 <cubebox51@gmail.com>
2016-12-20 17:39:09 -0800

    Update game.py

--- game.py
+++ game.py
@@ -1,27 +1,42 @@
-user_input = input("Whats 9 + 10")
-
-if (user_input) == 21:
-    print("You Stupid")
-
-if (user_input) == 69:
-    print ("No I Dont")
-
-elif (user_input) == 19:
-    print("YOU SMART OMG")
-
-else:
-    print("You Smart")
-
-    user_input = input("Whats 9 + 10")
-
-if (user_input) == 21:
-    print("You Stupid")
-
-if (user_input) == 69:
-    print ("No I Dont")
-
-elif (user_input) == 19:
-    print("YOU SMART OMG")
-
-else:
-    print("You Smart")
+print "Hello welcome to Mcdonald's how may I help you"
+print "Type 1 for food"
+print "Type 2 for drinks"
+user_input = input ("Type 3 for money")
+if (user_input) == 1:
+    print "We have Hamburgers, fries, and cancer"
+    print "Type 1 for hamburgers"
+    print "Type 2 for fries"
+    user_input = input ("Type 3 cancer")
+    if (user_input) == 1:
+        print "That will cost you $1.25"
+        print "Type 1 to pay"
+        user_input = input ("Type 2 to steal")
+        if (user_input) == 1:
+            user_input = input ("Thank you for comming")
+        elif (user_input) == 2:
+           user_input = input ("HEY COME BACK AND PAY!") 
+    elif (user_input) == 2:
+        user_input = input ("Potato!")
+    elif (user_input) == 3:
+        user_input = input ("Sorry we are out of cancer")
+if (user_input) == 2:
+    print "We have Coke, and Pepsi"
+    print "Type 1 for Coke"
+    user_input = input ("Type 2 for Pepsi")
+    if (user_input) == 1:
+        user_input = input ("COKE SUCKS NO DRINKS FOR YOU!!!!!")
+    elif (user_input) == 2: 
+        print "Nice I LOVE Pepsi Coke sucks that will cost you $1.00"
+        print "Type 1 to pay"
+        user_input = input ("Type 2 to steal")
+        if (user_input) == 2:
+            user_input = input ("HEY COME BACK AND PAY!")
+if (user_input) == 3:
+    print "So heres the plan you take the register ill destract the others."
+    print "Type 1 To call the cops"
+    user_input = input ("Type 2 to do the plan")
+    if (user_input) == 1:
+        user_input = input ("You got $150 for calling the cops")
+    elif (user_input) == 2:
+        user_input = input ("You got $150 for doing the plan")
+        
