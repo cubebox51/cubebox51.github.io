8203d829c3fcadf1a830daf09da935d7490eb744
cubebox51 <cubebox51@gmail.com>
2016-12-21 12:20:42 -0800

    Update game.py

--- game.py
+++ game.py
@@ -12,35 +12,35 @@ if int(user_input) == 1:
         print ("Type 1 to pay")
         user_input = input ("Type 2 to steal")
         if int(user_input) == 1:
-            user_input = input ("Thank you for comming")
+            print ("Thank you for comming")
         elif int(user_input) == 2:
-            user_input = input ("HEY COME BACK AND PAY!") 
+            print ("HEY COME BACK AND PAY!") 
     elif int(user_input) == 2:
-        user_input = input ("Potato!")
+        print ("Potato!")
     elif int(user_input) == 3:
-        user_input = input ("Sorry we are out of cancer")
+        print ("Sorry we are out of cancer")
 elif int(user_input) == 2:
     print ("We have Coke, and Pepsi")
     print ("Type 1 for Coke")
     user_input = input ("Type 2 for Pepsi")
     if int(user_input) == 1:
-        user_input = input ("COKE SUCKS NO DRINKS FOR YOU!!!!!")
+        print ("COKE SUCKS NO DRINKS FOR YOU!!!!!")
     elif int(user_input) == 2: 
         print ("Nice I LOVE Pepsi Coke sucks that will cost you $1.00")
         print ("Type 1 to pay")
         user_input = input ("Type 2 to steal")
         if int(user_input) == 1:
-            user_input = input ("Thank you for comming")
+            print ("Thank you for comming")
         elif int(user_input) == 2:
-            user_input = input ("HEY COME BACK AND PAY!")
+            print ("HEY COME BACK AND PAY!")
 elif int(user_input) == 3:
     print ("So heres the plan you take the register ill destract the others.")
     print ("Type 1 To call the cops")
     user_input = input ("Type 2 to do the plan")
     if int(user_input) == 1:
-        user_input = input ("You got $150 for calling the cops")
+        print ("You got $150 for calling the cops")
     elif int(user_input) == 2:
-        user_input = input ("You got $150 for doing the plan")
+        print ("You got $150 for doing the plan")
 else:
     print ("Sorry I did not get your order")
 input("Press Enter To Exit")
